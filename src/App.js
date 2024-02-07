d43bf34d3c458b3dc037e56dbdf21ef9c8f40576
Sanchose <sashapronehoda@gmail.com>
2024-02-07 16:42:44 +0100

    UseEffect commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import React, { useState } from 'react';
+import './index.scss';
+import { Success } from './components/Success';
+import { Users } from './components/Users';
+import { useEffect } from 'react';
+
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+    .then(res => res.json())
+    .then(json => {
+      setUsers(json.data)
+    }).catch(err => {
+      console.warn(err);
+      alert('Error whe we become a user')
+    })
+  }, [])
   return (
     <div className="App">
+      <Users />
+      {/* <Success /> */}
     </div>
   );
 }
