a66e33904c8ee76173aa65f12b7d1e2820732375
Sanchose <sashapronehoda@gmail.com>
2024-02-07 19:38:03 +0100

    added list and functions for adding people to the list

--- src/App.js
+++ src/App.js
@@ -6,23 +6,47 @@ import { useEffect } from 'react';
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState('');
+  const [invites, setInvites] = useState([]);
 
   useEffect(() => {
     fetch('https://reqres.in/api/users')
-    .then(res => res.json())
-    .then(json => {
-      setUsers(json.data)
-    }).catch(err => {
-      console.warn(err);
-      alert('Error whe we become a user')
-    })
+      .then(res => res.json())
+      .then(json => {
+        setUsers(json.data)
+      }).catch(err => {
+        console.warn(err);
+        alert('Error whe we become a user')
+      }).finally(() => setLoading(false))
   }, [])
-  return (
-    <div className="App">
-      <Users />
-      {/* <Success /> */}
-    </div>
-  );
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  }
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites(prev => prev.filter(_id => _id !== id));
+    }
+    else{
+      setInvites(prev => [...prev, id]);
+    }
+}
+
+return (
+  <div className="App">
+    <Users
+      searchValue={searchValue}
+      items={users}
+      isLoading={isLoading}
+      onChangeSearchValue={onChangeSearchValue}
+      invites={invites}
+      onClickInvite={onClickInvite}
+    />
+    {/* <Success /> */}
+  </div>
+);
 }
 
 export default App;
